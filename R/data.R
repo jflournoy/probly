f4f69618a37be8598d617114b324e0faa6f24036
John Flournoy <uoflournoy@gmail.com>
2018-03-22 15:34:13 -0400

    update, now with datas

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Social Probabilistic Learning Task Data
+#'
+#' Description
+#'
+#' @format A data frame with M rows and N variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+"splt_clean"
+
+#' Social Probabilistic Learning Confidence Data
+#'
+#' Description
+#'
+#' @format A data frame with M rows and N variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+"metacog_clean"
