c2117fa06b67882e14a7561f378e5ad3e281f0e6
John Flournoy <uoflournoy@gmail.com>
2018-03-25 21:04:56 -0400

    data

--- inst/batchtools/run_via_slurm.R
+++ inst/batchtools/run_via_slurm.R
@@ -5,11 +5,13 @@ plan(batchtools_slurm,
      template = system.file('batchtools', 'batchtools.slurm.tmpl', package = 'probly'),
      resources = list(ncpus = 4, walltime = 60*24-1, memory = '1G'))
 
+data_dir <- '/home/flournoy/otherhome/data/splt/probly/'
+if(!file.exists(data_dir)){
+    stop('Data directory "', data_dir, '" does not exist')
+}
+
 fit_size_f <- future({
-    data_dir <- '/home/flournoy/otherhome/data/splt/probly/'
-    if(!file.exists(data_dir)){
-        stop('Data dir "', data_dir, '" does not exist')
-    }
+    message("Data goes here: ", data_dir)
     source(system.file('r_aux', 'simulate_data_from_splt.R', package = 'probly'))
     source(system.file('r_aux', 'estimate_rl_from_sim.R', package = 'probly'))
 })
