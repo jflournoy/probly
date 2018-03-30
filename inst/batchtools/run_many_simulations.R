8bc044899bb1593ba4b5f2352bcdeb593d6f32e9
John Flournoy <uoflournoy@gmail.com>
2018-03-30 14:13:42 -0400

    preempt okay

--- inst/batchtools/run_many_simulations.R
+++ inst/batchtools/run_many_simulations.R
@@ -8,7 +8,7 @@ library(listenv)
 plan(batchtools_slurm,
      template = system.file('batchtools', 'batchtools.slurm.tmpl', package = 'probly'),
      resources = list(ncpus = 4, walltime = 60*24-1, memory = '1G',
-                      partitions = 'short,fat,long,longfat'))
+                      partitions = 'short,fat,long,longfat,preempt'))
 
 data_dir <- '/home/flournoy/otherhome/data/splt/probly/'
 data_dir_indvsims <- file.path(data_dir, 'indiv_sim_saves')
