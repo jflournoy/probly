c6fad30b9131f7c60bd491612d65d98d7aaf8be7
John Flournoy <uoflournoy@gmail.com>
2018-04-19 09:22:49 -0400

    wip

--- inst/batchtools/run_many_simulations.R
+++ inst/batchtools/run_many_simulations.R
@@ -7,103 +7,81 @@ library(listenv)
 
 plan(batchtools_slurm,
      template = system.file('batchtools', 'batchtools.slurm.tmpl', package = 'probly'),
-     resources = list(ncpus = 4, walltime = 60*24*4, memory = '1G',
+     resources = list(ncpus = 6, walltime = 60*24*4, memory = '1G',
                       partitions = 'long,longfat'))
 
 data_dir <- '/home/flournoy/otherhome/data/splt/probly/'
-data_dir_indvsims <- file.path(data_dir, 'indiv_sim_saves')
 if(!file.exists(data_dir)){
     stop('Data directory "', data_dir, '" does not exist')
 } else {
     message("Data goes here: ", data_dir)
 }
-if(!file.exists(data_dir_indvsims)){
-    stop('Individual simulation directory "', data_dir_indvsims, '" does not exist')
-} else {
-    message("Individual simulation results go here: ", data_dir_indvsims)
-}
 
-data(splt)
-splt_no_na <- splt[!is.na(splt$pressed_r), ]
-splt_no_na$proportion_fac <- factor(splt_no_na$proportion, levels = c('80_20', '20_80'))
-optimal_side_mat <- get_col_as_trial_matrix(splt_no_na, col = 'proportion_fac')
-
-mu_xi <- c(-2.7, -2.6, -2.4)
-mu_b <- c(-.07, .28, .39)
-mu_eps <- c(-1.65, -1.65 + .3, -1.65 + .2)
-mu_rho <- c(-.3, -.3 + .35, -.3 + .45)
-
-task_structure <- probly::make_task_structure_from_data(splt_no_na)
-task_structure_stan <- task_structure #just to sub NA with -1
-task_structure_stan$condition[is.na(task_structure_stan$condition)] <- -1
-task_structure_stan$cue[is.na(task_structure_stan$cue)] <- -1
-
-stan_sim_data <- list(
-    N = task_structure_stan$N,
-    `T` = max(task_structure_stan$Tsubj),
-    K = task_structure_stan$K,
-    M = task_structure_stan$M,
-    ncue = task_structure_stan$n_cues,
-    mm = task_structure_stan$mm,
-    Tsubj = task_structure_stan$Tsubj,
-    condition = task_structure_stan$condition,
-    cue = task_structure_stan$cue
+model_filename_list <- list(
+    # intercept_only = system.file('stan', 'splt_intercept_only.stan', package = 'probly'),
+    rl_2_level = system.file('stan', 'splt_rl_2_level.stan', package = 'probly'),
+    rl_2_level_no_b = system.file('stan', 'splt_rl_2_level_no_b.stan', package = 'probly'),
+    rl_repar_exp = system.file('stan', 'splt_rl_reparam_exp.stan', package = 'probly'),
+    rl_repar_exp_no_b = system.file('stan', 'splt_rl_reparam_exp_no_b.stan', package = 'probly')
 )
-
-stan_rl_fn <- system.file('stan', 'splt_rl_reparam.stan', package = 'probly')
 
 iter <- 100
 results_f <- listenv()
 
-for(i in 1:iter){
-    results_f[[i]] %<-% {
-        library(rstan)
-        stan_sim_length <- length(stan_sim_data)
-        task_data <- probly::simulate_splt_data(task_structure,
-                                                mu_xi = mu_xi,
-                                                mu_b = mu_b,
-                                                mu_eps = mu_eps,
-                                                mu_rho = mu_rho)
-        task_data_stan <- task_data$task_behavior
-        task_data_stan$outcome_realized[is.na(task_data_stan$outcome_realized)] <- -1
-        task_data_stan$press_right[is.na(task_data_stan$press_right)] <- -1
+for(mod in seq_along(model_filename_list)){
+    print(paste0('Generating simulated behavior for: ', names(model_filename_list)[mod]))
+    for(i in 1:iter){
+        results_f[[i]] %<-% {
+            library(rstan)
+            stan_sim_length <- length(stan_sim_data)
+            task_data <- probly::simulate_splt_data(task_structure,
+                                                    mu_xi = mu_xi,
+                                                    mu_b = mu_b,
+                                                    mu_eps = mu_eps,
+                                                    mu_rho = mu_rho)
+            task_data_stan <- task_data$task_behavior
+            task_data_stan$outcome_realized[is.na(task_data_stan$outcome_realized)] <- -1
+            task_data_stan$press_right[is.na(task_data_stan$press_right)] <- -1
 
-        stan_sim_data$press_right = task_data_stan$press_right
-        stan_sim_data$outcome = task_data_stan$outcome
+            stan_sim_data$press_right = task_data_stan$press_right
+            stan_sim_data$outcome = task_data_stan$outcome
 
-        trial_num <- -(dim(optimal_side_mat)[2]-1):0
-        prop_optimal_resp <- 100*apply(
-            task_data$task_behavior$press_right == (optimal_side_mat - 1),
-            2, mean, na.rm = T)
-        learning_traj <- coef(summary(lm(prop_optimal_resp ~ trial_num)))[,'Estimate']
+            trial_num <- -(dim(optimal_side_mat)[2]-1):0
+            prop_optimal_resp <- 100*apply(
+                task_data$task_behavior$press_right == (optimal_side_mat - 1),
+                2, mean, na.rm = T)
+            learning_traj <- coef(summary(lm(prop_optimal_resp ~ trial_num)))[,'Estimate']
 
-        # ##--TESTING
-        # devtools::install_local('~/code_new/probly')
-        # stan_rl_fn <- '~/code_new/probly/inst/stan/splt_rl_reparam.stan'
-        # stan_model_thing <- rstan::stan_model(file = stan_rl_fn)
-        # stan_optim <- rstan::optimizing(stan_model_thing, data = stan_sim_data)
-        # stan_optim_parnames <- grep('mu', names(stan_optim$par), value = T)
-        # stan_optim$par[stan_optim_parnames] - c(mu_xi, mu_eps, mu_b, mu_rho)
-        # stan_vb <- rstan::vb(stan_model_thing, data = stan_sim_data)
-        # summary(stan_vb, pars = stan_optim_parnames)
-        # ##--TESTING
+            # ##--TESTING
+            # devtools::install_local('~/code_new/probly')
+            # stan_rl_fn <- '~/code_new/probly/inst/stan/splt_rl_reparam.stan'
+            # stan_model_thing <- rstan::stan_model(file = stan_rl_fn)
+            # stan_optim <- rstan::optimizing(stan_model_thing, data = stan_sim_data)
+            # stan_optim_parnames <- grep('mu', names(stan_optim$par), value = T)
+            # stan_optim$par[stan_optim_parnames] - c(mu_xi, mu_eps, mu_b, mu_rho)
+            # stan_vb <- rstan::vb(stan_model_thing, data = stan_sim_data)
+            # summary(stan_vb, pars = stan_optim_parnames)
+            # ##--TESTING
 
-        stanFit <- rstan::stan(file = stan_rl_fn,
-                               data = stan_sim_data,
-                               chains = 4, cores = 4,
-                               iter = 1500, warmup = 1000,
-                               control = list(max_treedepth = 15, adapt_delta = 0.99))
+            stanFit <- rstan::stan(file = stan_rl_fn,
+                                   data = stan_sim_data,
+                                   chains = 4, cores = 4,
+                                   iter = 1500, warmup = 1000,
+                                   control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-        model_summary <- rstan::summary(stanFit)
-        sampler_params <- rstan::get_sampler_params(stanFit)
+            model_summary <- rstan::summary(stanFit)
+            sampler_params <- rstan::get_sampler_params(stanFit)
 
-        return_list <- list(fit_summary = model_summary,
-                            sampler_params = sampler_params,
-                            coefs = learning_traj,
-                            data = task_data)
-        saveRDS(return_list, file = file.path(data_dir_indvsims,
-                                              paste0('sim_', sprintf("%03d",i), '.RDS')))
-        return_list
+            return_list <- list(fit_summary = model_summary,
+                                sampler_params = sampler_params,
+                                coefs = learning_traj,
+                                data = task_data)
+
+            saveRDS(stanFit,
+                    file.path(data_dir,
+                              paste0('splt-tght-', names(model_filename_list)[mod],
+                                     '-', round(as.numeric(Sys.time())/1000,0),'.RDS')))
+        }
     }
 }
 
