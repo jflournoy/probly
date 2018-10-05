26d5c2aa35d5afbae127ef7fd74bba0b9a4257ba
John Flournoy <uoflournoy@gmail.com>
2018-10-05 18:22:22 -0400

    add predictors directly in model

---
+++ inst/batchtools/run_rl_models_w_preds.R
@@ -0,0 +1,222 @@
+## ---- run_baseline_models
+
+library(future)
+library(future.batchtools)
+library(listenv)
+library(rstan)
+
+if(grepl('(^n\\d|talapas-ln1)', system('hostname', intern = T))){
+    niter <- 2000
+    nchains <- 6
+    niterperchain <- ceiling(niter/nchains)
+    warmup <- 1200
+    data_dir <- '/gpfs/projects/dsnlab/flournoy/data/splt/probly'
+    plan(batchtools_slurm,
+         template = system.file('batchtools', 'batchtools.slurm.tmpl', package = 'probly'),
+         resources = list(ncpus = 6, walltime = 60*24*8, memory = '4G',
+                          partitions = 'long,longfat'))
+    AWS = F
+} else if(grepl('^ip-', system('hostname', intern = T))) { #AWS
+    niter <- 2000
+    nchains <- 4
+    niterperchain <- ceiling(niter/nchains)
+    warmup <- 1250
+    data_dir <- '/home/ubuntu/data'
+    if(!any(grepl('WHICH_MOD', ls()))) stop("var WHICH_MOD must be set on AWS")
+    AWS = T
+    devtools::install_github('jflournoy/probly')
+} else {
+    data_dir <- '/data/jflournoy/split/probly'
+    niter <- 1
+    nchains <- 2
+    warmup <- 1
+    niterperchain <- ceiling(niter/nchains)
+    plan(tweak(multiprocess, gc = T, workers = 8))
+    AWS = F
+}
+
+library(probly)
+
+if(!file.exists(data_dir)){
+    stop('Data directory "', data_dir, '" does not exist')
+} else {
+    message("Data goes here: ", data_dir)
+}
+
+model_filename_list <- list(
+    rl_2_level_preds = system.file('stan', 'splt_rl_2_level_w2lpreds.stan', package = 'probly')
+)
+
+pop_parlist <- c('mu_delta_ep', 'mu_delta_xi',
+                 'tau_ep', 'tau_xi',
+                 'L_Omega_xi', 'L_Omega_ep')
+indiv_parlist <- c('beta_ep_prm', 'beta_xi_prm', 'pR_final', 'log_lik')
+
+pop_parlist_rho <- c('mu_delta_rho', 'tau_rho', 'L_Omega_rho')
+indiv_parlist_rho <- c('beta_rho_prm')
+
+pop_parlist_b <- c('mu_delta_b', 'tau_b', 'L_Omega_b')
+indiv_parlist_b <- c('beta_b')
+
+save_pars_list <- list(
+    rl_2_level_preds = c(pop_parlist, indiv_parlist, pop_parlist_rho, indiv_parlist_rho, pop_parlist_b, indiv_parlist_b)
+)
+
+if(AWS){
+    append_to_data_fn <- names(model_filename_list)[[WHICH_MOD]]
+    model_filename_list <- model_filename_list[WHICH_MOD] #one mod per instance here.
+} else {
+    append_to_data_fn <- ''
+}
+
+data(splt)
+data(splt_dev_and_demog)
+data(splt_fsmi)
+
+college_rubric_dir <- system.file('scoring_rubrics', 'college', package = 'probly')
+ksrq_rubric <- scorequaltrics::get_rubrics(
+    rubric_filenames = dplyr::data_frame(
+        file = file.path(college_rubric_dir, 'K-SRQ_scoring_rubric.csv')),
+    type = 'scoring')
+ksrq_key <- scorequaltrics::create_key_from_rubric(ksrq_rubric)
+
+splt_ksrq_mate_stat <- as.data.frame(psych::scoreItems(
+    ksrq_key[c('k_srq_sexual_relationships', 'k_srq_admiration')],
+    splt_fsmi, missing = TRUE, impute = 'none')$scores)
+splt_ksrq_mate_stat$SID <- splt_fsmi$SID
+
+print(dim(splt))
+splt_no_na <- splt[!is.na(splt$pressed_r), ]
+print(dim(splt_no_na))
+splt_no_na_dev <- dplyr::left_join(
+    splt_no_na,
+    dplyr::distinct(splt_dev_and_demog,
+                    SID, age, PDS_mean_score, gender),
+    by = c('id' = 'SID'))
+print(dim(splt_no_na_dev))
+splt_no_na_dev_matestat <- dplyr::left_join(
+    splt_no_na_dev,
+    splt_ksrq_mate_stat,
+    by = c('id' = 'SID'))
+print(dim(splt_no_na_dev_matestat))
+print(dim(dplyr::distinct(splt_no_na_dev_matestat, id)))
+# print(dim(dplyr::distinct(dplyr::filter(
+#     splt_no_na_dev_matestat,
+#     !is.na(age), !is.na(PDS_mean_score),
+#     !is.na(k_srq_sexual_relationships),
+#     !is.na(k_srq_admiration)), id)))
+
+splt <- splt_no_na_dev_matestat[!is.na(splt_no_na_dev_matestat$age) &
+                                    !is.na(splt_no_na_dev_matestat$gender) &
+                                    splt_no_na_dev_matestat$age < 30, ]
+
+na_age <- is.na(unique(splt[,c('id', 'age')])$age)
+na_gender <- is.na(unique(splt[,c('id', 'gender')])$gender)
+na_pds <- is.na(unique(splt[,c('id', 'PDS_mean_score')])$PDS_mean_score)
+age_sd <- sd(unique(splt[,c('id', 'age')])$age[unique(splt[,c('id', 'age')])$age < 30], na.rm = T)
+age_mean <- mean(unique(splt[,c('id', 'age')])$age[unique(splt[,c('id', 'age')])$age < 30], na.rm = T)
+pds_sd <- sd(as.numeric(unique(splt[,c('id', 'PDS_mean_score')])$PDS_mean_score), na.rm = T)
+pds_mean <- mean(as.numeric(unique(splt[,c('id', 'PDS_mean_score')])$PDS_mean_score), na.rm = T)
+splt$age_std <- (splt$age - age_mean)/age_sd
+splt$pds_std <- (as.numeric(splt$PDS_mean_score) - pds_mean)/pds_sd
+splt$gender_c <- as.numeric(splt$gender) - .5 #0 or -0.5 = male, 1 or .5 = female
+
+gender_c <- get_col_as_trial_matrix(splt, 'gender_c', id_col = 'id', sample_col = 'sample', trial_col = 'trial_index')[,1]
+age_std <- get_col_as_trial_matrix(splt, 'age_std', id_col = 'id', sample_col = 'sample', trial_col = 'trial_index')[,1]
+age_x_gender <- age_std * gender_c
+
+Xj <- model.matrix(~1+age_std+gender_c+age_x_gender)
+J_pred <- dim(Xj)[2]
+
+fit_many_mods_f <- listenv()
+
+for(mod in 1:length(model_filename_list)){
+    print(paste0('Sampling from model: ', names(model_filename_list)[mod]))
+    fit_many_mods_f[[mod]]  %<-% {
+        library(rstan)
+        library(probly)
+
+        dim(splt)
+        splt_no_na <- splt
+
+        splt_no_na$opt_is_right <- as.numeric(factor(splt_no_na$proportion,
+                                                     levels = c('80_20', '20_80'))) - 1
+        splt_no_na$press_opt <- as.numeric(splt_no_na$opt_is_right == splt_no_na$pressed_r)
+
+        task_structure <- probly::make_task_structure_from_data(splt_no_na)
+        task_structure$condition[is.na(task_structure$condition)] <- -1
+        task_structure$cue[is.na(task_structure$cue)] <- -1
+
+        outcome <- probly::get_col_as_trial_matrix(
+            splt_no_na,
+            col = 'outcome', id_col = 'id',
+            sample_col = 'sample', trial_col = 'trial_index')
+
+        press_right <- probly::get_col_as_trial_matrix(
+            splt_no_na,
+            col = 'pressed_r', id_col = 'id',
+            sample_col = 'sample', trial_col = 'trial_index')
+
+        press_opt <- probly::get_col_as_trial_matrix(
+            splt_no_na,
+            col = 'press_opt', id_col = 'id',
+            sample_col = 'sample', trial_col = 'trial_index')
+
+        outcome[is.na(outcome)] <- -1
+        outcome_r <- outcome_l <- outcome
+        outcome_r[] <- outcome_l[] <- -1
+
+        press_right[is.na(press_right)] <- -1
+        press_opt[is.na(press_opt)] <- -1
+
+        stan_data <- list(
+            N = task_structure$N,
+            `T` = max(task_structure$Tsubj),
+            K = task_structure$K,
+            M = task_structure$M,
+            J = J_pred,
+            ncue = task_structure$n_cues,
+            mm = task_structure$mm,
+            Tsubj = task_structure$Tsubj,
+            condition = task_structure$condition,
+            cue = task_structure$cue,
+            press_right = press_right,
+            outcome = outcome,
+            outcome_r = outcome_r,
+            outcome_l = outcome_l,
+            u = Xj,
+            run_estimation = 1
+        )
+
+        if(names(model_filename_list)[mod] == 'intercept_only'){
+            stan_data$press_right <- NULL
+            stan_data$press_opt <- press_opt
+        }
+
+        save_pars <- save_pars_list[[mod]]
+
+        message('Model: ', names(model_filename_list)[[mod]])
+        message('Save pars: ', save_pars)
+
+        stanFit <- rstan::stan(file = model_filename_list[[mod]],
+                               data = stan_data,
+                               chains = nchains, cores = nchains,
+                               iter = warmup + niterperchain, warmup = warmup,
+                               pars = save_pars, include = TRUE,
+                               control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+        savepath <- file.path(data_dir,
+                              paste0('splt-preds-', names(model_filename_list)[mod],
+                                     '-', round(as.numeric(Sys.time())/1000,0),'.RDS'))
+
+        message('Saving fit to ', savepath)
+        message('Size: ', format(object.size(stanFit), units = 'GB'))
+
+        saveRDS(stanFit, savepath)
+        list(complete = TRUE)
+    }
+}
+
+saveRDS(splt,
+        file.path(data_dir, paste0('splt-preds-data-', append_to_data_fn, '-',
+                                   round(as.numeric(Sys.time())/1000,0),'.RDS')))
---
+++ inst/stan/splt_rl_2_level_w2lpreds.stan
@@ -0,0 +1,202 @@
+data {
+    int<lower=1> N; //number of subjects
+    int<lower=1> T; //max number of trials
+    int<lower=2> K; //number conditions
+    int<lower=1> ncue; //number of cues
+    int<lower=1, upper=T> Tsubj[N]; //trials per subject
+    int<lower=-1,upper=3> condition[N,T]; //Conditions 1-3
+    int<lower=1> J; //number if subject-level predictors _including intercept_.
+
+    real outcome[N, T]; //reward for the trial -- either 0, 1, or 5 points
+    real outcome_r[N, T]; //for simulated data - reward for the trial -- either 0, 1, or 5 points
+    real outcome_l[N, T]; //for simulated data - reward for the trial -- either 0, 1, or 5 points
+    int<lower=-1, upper=1> press_right[N, T]; //choices "0" = left, "1" = right
+    int<lower=-1, upper=ncue> cue[N, T]; //which cue appeared
+    matrix[N, J] u; //N-level predictors. for adding, e.g., age and puberty _INCLUDE INTERCEPT_
+
+    int<lower=0, upper=1> run_estimation; // a switch to evaluate the likelihood
+}
+
+transformed data {
+    vector[ncue] initV;
+    initV  = rep_vector(0.0, ncue);
+}
+
+parameters {
+    matrix[K, N] z_xi; //deviations from separate intercept for each condition, k in 1:K
+    matrix[K, N] z_ep;
+    matrix[K, N] z_b;
+    matrix[K, N] z_rho;
+
+    cholesky_factor_corr[K] L_Omega_xi; //for the correlation among intercepts across conditions
+    cholesky_factor_corr[K] L_Omega_ep;
+    cholesky_factor_corr[K] L_Omega_b;
+    cholesky_factor_corr[K] L_Omega_rho;
+
+    vector<lower=0>[K] tau_xi; //scaling of the correlations
+    vector<lower=0>[K] tau_ep;
+    vector<lower=0>[K] tau_b;
+    vector<lower=0>[K] tau_rho;
+
+    matrix[J,K] mu_delta_xi; //overall mean for each condition
+    matrix[J,K] mu_delta_ep;
+    matrix[J,K] mu_delta_b;
+    matrix[J,K] mu_delta_rho;
+}
+
+transformed parameters {
+    matrix<lower=0, upper=1>[N, K] beta_xi_prm; //per-individual coefficients for xi, for each condition, transformed
+    matrix<lower=0, upper=1>[N, K] beta_ep_prm; //per-individual coefficients for ep, for each condition, transformed
+    matrix[N, K] beta_b; //per-individual coefficients for b, for each condition
+    matrix<lower=0>[N, K] beta_rho_prm; //per-individual coefficients for rho, for each condition, transformed
+
+    beta_xi_prm = Phi_approx(u * mu_delta_xi + (diag_pre_multiply(tau_xi, L_Omega_xi) * z_xi)');
+    beta_ep_prm = Phi_approx(u * mu_delta_ep + (diag_pre_multiply(tau_ep, L_Omega_ep) * z_ep)');
+    beta_b = u * mu_delta_b + (diag_pre_multiply(tau_b, L_Omega_b) * z_b)';
+    beta_rho_prm = exp(u * mu_delta_rho + (diag_pre_multiply(tau_rho, L_Omega_rho) * z_rho)');
+}
+
+model {
+    // gng_m2: RW + noise + bias model in Guitart-Masip et al 2012
+    // hyper parameters
+    to_vector(mu_delta_xi)  ~ normal(0, 1);
+    to_vector(mu_delta_ep)  ~ normal(0, 1);
+    to_vector(mu_delta_b)  ~ normal(0, 2);
+    to_vector(mu_delta_rho)  ~ normal(0, 1);
+
+    //Sigma_param for individual level coefficients
+    to_vector(z_xi) ~ normal(0, 1);
+    to_vector(z_ep) ~ normal(0, 1);
+    to_vector(z_b) ~ normal(0, 1);
+    to_vector(z_rho) ~ normal(0, 1);
+    tau_xi ~ exponential(1);
+    tau_ep ~ exponential(1);
+    tau_rho ~ exponential(1);
+    tau_b ~  exponential(1);
+    L_Omega_xi ~ lkj_corr_cholesky(2);
+    L_Omega_ep ~ lkj_corr_cholesky(2);
+    L_Omega_b ~ lkj_corr_cholesky(2);
+    L_Omega_rho ~ lkj_corr_cholesky(2);
+
+    if(run_estimation == 1){
+        for (i in 1:N) {
+            vector[ncue] wv_r;  // action wegith for go
+            vector[ncue] wv_l; // action wegith for nogo
+            vector[ncue] qv_r;  // Q value for go
+            vector[ncue] qv_l; // Q value for nogo
+            vector[ncue] pR;   // prob of go (press)
+            real beta_xi_it;
+            real beta_ep_it;
+            real beta_rho_it;
+            real beta_b_it;
+
+            wv_r  = initV;
+            wv_l = initV;
+            qv_r  = initV;
+            qv_l = initV;
+
+            for (t in 1:Tsubj[i]) {
+                beta_xi_it = beta_xi_prm[i, condition[i, t]];
+                beta_ep_it = beta_ep_prm[i, condition[i, t]];
+                beta_rho_it = beta_rho_prm[i, condition[i, t]];
+                beta_b_it = beta_b[i, condition[i, t]];
+
+                // print(beta_xi_it);
+                // print(beta_ep_it);
+                // print(beta_rho_it);
+                // print(beta_b_it);
+
+                wv_r[cue[i, t]]  = qv_r[cue[i, t]] + beta_b_it;
+                wv_l[cue[i, t]] = qv_l[cue[i, t]];  // qv_l is always equal to wv_l (regardless of action)
+                pR[cue[i, t]]   = inv_logit(wv_r[cue[i, t]] - wv_l[cue[i, t]]);
+                pR[cue[i, t]]   = pR[cue[i, t]] * (1 - beta_xi_it) + beta_xi_it/2;  // noise
+
+                // print(wv_r[cue[i, t]]);
+                // print(wv_l[cue[i, t]]);
+                // print(pR[cue[i, t]]);
+
+                press_right[i, t] ~ bernoulli(pR[cue[i, t]]);
+
+                // update action values
+                if (press_right[i, t]) { // update go value
+                    qv_r[cue[i, t]]  = qv_r[cue[i, t]] + beta_ep_it * (beta_rho_it * outcome[i, t] - qv_r[cue[i, t]]);
+                } else { // update no-go value
+                    qv_l[cue[i, t]] = qv_l[cue[i, t]] + beta_ep_it * (beta_rho_it * outcome[i, t] - qv_l[cue[i, t]]);
+                }
+            } // end of t loop
+        } // end of i loop
+    } // end estimate check
+}
+
+generated quantities {
+    vector[N] log_lik;
+    int<lower=-1, upper=1> pright_pred[N, T]; //choices "0" = left, "1" = right
+
+    //save final pR
+    vector[ncue] pR_final[N];
+
+    for (i in 1:N) {
+        for (t in 1:T) {
+            pright_pred[i, t] = -1;
+        }
+    }
+
+    { //local to save space and time
+    for (i in 1:N) {
+        vector[ncue] wv_r;  // action wegith for go
+        vector[ncue] wv_l; // action wegith for nogo
+        vector[ncue] qv_r;  // Q value for go
+        vector[ncue] qv_l; // Q value for nogo
+        vector[ncue] pR;   // prob of go (press)
+        real beta_xi_it;
+        real beta_ep_it;
+        real beta_rho_it;
+        real beta_b_it;
+        vector[T] log_lik_iters;
+
+        wv_r  = initV;
+        wv_l = initV;
+        qv_r  = initV;
+        qv_l = initV;
+
+        log_lik[i] = 0;
+        log_lik_iters = rep_vector(0, T);
+
+        for (t in 1:Tsubj[i]) {
+            //neater code
+            beta_xi_it = beta_xi_prm[i, condition[i, t]];
+            beta_ep_it = beta_ep_prm[i, condition[i, t]];
+            beta_rho_it = beta_rho_prm[i, condition[i, t]];
+            beta_b_it = beta_b[i, condition[i, t]];
+
+            wv_r[cue[i, t]]  = qv_r[cue[i, t]] + beta_b_it;
+            wv_l[cue[i, t]] = qv_l[cue[i, t]];  // qv_l is always equal to wv_l (regardless of action)
+            pR[cue[i, t]]   = inv_logit(wv_r[cue[i, t]] - wv_l[cue[i, t]]);
+            pR[cue[i, t]]   = pR[cue[i, t]] * (1 - beta_xi_it) + beta_xi_it/2;  // noise
+
+            pright_pred[i, t] = bernoulli_rng(pR[cue[i, t]]);
+
+            if(run_estimation == 1){
+                log_lik_iters[t] = bernoulli_lpmf(press_right[i, t] | pR[cue[i, t]]);
+
+                // update action values
+                if (press_right[i, t]) { // update go value
+                    qv_r[cue[i, t]]  = qv_r[cue[i, t]] + beta_ep_it * (beta_rho_it * outcome[i, t] - qv_r[cue[i, t]]);
+                } else { // update no-go value
+                    qv_l[cue[i, t]] = qv_l[cue[i, t]] + beta_ep_it * (beta_rho_it * outcome[i, t] - qv_l[cue[i, t]]);
+                }
+            } else {
+                log_lik_iters[t] = bernoulli_lpmf(pright_pred[i, t] | pR[cue[i, t]]);
+
+                if (pright_pred[i, t]) { // update go value
+                    qv_r[cue[i, t]]  = qv_r[cue[i, t]] + beta_ep_it * (beta_rho_it * outcome_r[i, t] - qv_r[cue[i, t]]);
+                } else { // update no-go value
+                    qv_l[cue[i, t]] = qv_l[cue[i, t]] + beta_ep_it * (beta_rho_it * outcome_l[i, t] - qv_l[cue[i, t]]);
+                }
+            }
+        } // end of t loop
+        log_lik[i] = sum(log_lik_iters);
+        pR_final[i] = pR;
+    } // end of i loop
+    }
+}
